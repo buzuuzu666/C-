b2937ae1ef5605fc6069a29319397c1db72f1f6f
Elizaveta <liza-kalinina@bk.ru>
2023-02-02 12:04:37 +0300

    example 009 method array

---
+++ example009_methodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1, 12, 31, 4, 51, 18, 6, 17, 18, 9};
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.Write($"{array[index]} ");
+        // break;
+    }
+
+    index++;
+}
